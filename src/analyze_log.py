3338b0bba404ca997077752ee58e6cfdebe059af
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-12-15 15:37:21 -0300

    feat: implemented maria_most_ordered function on analyze_log module

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,6 +1,12 @@
 import csv
 
 
+def analyze_log(path_to_file):
+    data = read(path_to_file)
+    most_ordered_maria = maria_most_ordered(data)
+    return most_ordered_maria
+
+
 def read(path_to_file):
     if not path_to_file.endswith('.csv'):
         raise ValueError(f"Extensão inválida: '{path_to_file}'")
@@ -14,10 +20,32 @@ def read(path_to_file):
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
 
 
-def analyze_log(path_to_file):
-    raise NotImplementedError
+def maria_most_ordered(data):
+    frequency = {}
+
+    for order in data:
+        if order["customer"] == 'maria':
+            if order["order"] not in frequency:
+                frequency[order["order"]] = 1
+            else:
+                frequency[order["order"]] += 1
+
+    # https://datagy.io/python-get-dictionary-key-with-max-value/
+    return max(frequency, key=frequency.get)
+
+
+def arnaldo_ordered_hamburguer(data):
+    ...
+
+
+def joao_never_tasted(data):
+    ...
+
+
+def joao_never_went(data):
+    ...
 
 
 if __name__ == "__main__":
-    print(read("data/orders_1.csv"))
-    # print(analyze_log("data/orders_1.csv"))
+    # print(read("data/orders_1.csv"))
+    print(analyze_log("data/orders_1.csv"))
