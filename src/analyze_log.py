b07e75dea425cbe8eed28f12c4f26c96374e1f8d
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-12-15 13:13:11 -0300

    feat: implemented a file reader function on analyze_log module

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,23 @@
+import csv
+
+
+def read(path_to_file):
+    if not path_to_file.endswith('.csv'):
+        raise ValueError(f"Extensão inválida: '{path_to_file}'")
+
+    try:
+        with open(path_to_file, 'r') as file:
+            file_reader = csv.DictReader(file, ["customer", "order", "day"])
+            orders = [row for row in file_reader]
+            return orders
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
+
+
+if __name__ == "__main__":
+    print(read("data/orders_1.csv"))
+    # print(analyze_log("data/orders_1.csv"))
